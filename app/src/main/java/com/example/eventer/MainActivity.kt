d35dec38de530df6e894a04c8caad6fcb8aea946
adnusan <ad.nusan@gmail.com>
2022-11-09 01:32:33 -0800

    added firebase dependencies

--- app/src/main/java/com/example/eventer/MainActivity.kt
+++ app/src/main/java/com/example/eventer/MainActivity.kt
@@ -16,14 +16,14 @@ class MainActivity : AppCompatActivity() {
         val login = findViewById<TextView>(R.id.login)
         val password = findViewById<TextView>(R.id.password)
         val loginButton = findViewById<Button>(R.id.loginButton)
-        val signup_link = findViewById<TextView>(R.id.signup_link)
+        val signupLink = findViewById<TextView>(R.id.signup_link)
 
         loginButton.setOnClickListener {
             Toast.makeText(this, "Login: ${login.text} Password: ${password.text}", Toast.LENGTH_SHORT).show()
 
         }
 
-        signup_link.setOnClickListener() {
+        signupLink.setOnClickListener {
             //TODO: Add intent to go to signup page
             Toast.makeText(this, "Sign Up", Toast.LENGTH_SHORT).show()
             val i = Intent(applicationContext, SignUp::class.java)
