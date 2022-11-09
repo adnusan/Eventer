510e3f3ae73c34d72680f29ee8477dcd283621a4
adnusan <ad.nusan@gmail.com>
2022-11-09 15:49:16 -0800

    database connected, able to write in firebase db

--- app/src/main/java/com/example/eventer/MainActivity.kt
+++ app/src/main/java/com/example/eventer/MainActivity.kt
@@ -6,8 +6,21 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var database: DatabaseReference
+    fun initializeDbRef() {
+        // [START initialize_database_ref]
+        database = Firebase.database.reference
+        database.child("user").setValue("nuu")
+
+        // [END initialize_database_ref]
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +33,7 @@ class MainActivity : AppCompatActivity() {
 
         loginButton.setOnClickListener {
             Toast.makeText(this, "Login: ${login.text} Password: ${password.text}", Toast.LENGTH_SHORT).show()
-
+            initializeDbRef()
         }
 
         signupLink.setOnClickListener {
